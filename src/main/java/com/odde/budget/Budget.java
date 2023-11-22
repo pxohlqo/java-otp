34cafa86f0e2e3d1d122c449b629225034d90233
Haoqin Xu <Haoqin.Xu@bmw.com>
2023-11-22 15:15:06 +0800

    Day 2: Refactoring

--- src/main/java/com/odde/budget/Budget.java
+++ src/main/java/com/odde/budget/Budget.java
@@ -1,10 +1,12 @@
 package com.odde.budget;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.time.YearMonth;
 
 public class Budget {
 
-    Budget(YearMonth yearMonth, long amount){
+    Budget(YearMonth yearMonth, long amount) {
         mYearMonth = yearMonth;
         mAmount = amount;
     }
@@ -12,11 +14,33 @@ public class Budget {
     private final YearMonth mYearMonth;
     private final long mAmount;
 
-    public YearMonth getYearMonth(){
+    long amountPerDay() {
+        int days = getYearMonth().lengthOfMonth();
+        return getAmount() / days;
+    }
+
+    LocalDate getBudgetEnd() {
+        return getYearMonth().atEndOfMonth();
+    }
+
+    LocalDate getBudgetStart() {
+        return getYearMonth().atDay(1);
+    }
+
+    public YearMonth getYearMonth() {
         return mYearMonth;
     }
 
-    public long getAmount(){
+    public long getAmount() {
         return mAmount;
+    }
+
+    long getOverlappingAmount(LocalDate startDate, LocalDate endDate) {
+        LocalDate budgetStart = this.getBudgetStart();
+        LocalDate budgetEnd = this.getBudgetEnd();
+        if (endDate.isBefore(budgetStart) || startDate.isAfter(budgetEnd)) return 0;
+        LocalDate start = startDate.isAfter(budgetStart) ? startDate : budgetStart;
+        LocalDate end = endDate.isBefore(budgetEnd) ? endDate : budgetEnd;
+        return this.amountPerDay() * (Period.between(start, end).getDays() + 1);
     }
 }
